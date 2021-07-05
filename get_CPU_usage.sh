00fa0a7bf1cc7ffcdf10ae7473ee40dadebcbb8e
tuj84257 <67998287+tuj84257@users.noreply.github.com>
2021-07-05 12:06:22 -0400

    Update get_CPU_usage.sh
    Better version that does not use `grep`.

--- get_CPU_usage.sh
+++ get_CPU_usage.sh
@@ -6,7 +6,8 @@ function get_CPU_usage {
     # get_CPU_usage <interval_in_seconds> <PID>
 
     while sleep $1; do
-         top -bn1 | grep $2 | gawk '{ print $9 }';
+         # extract only the process with the given PID from `top`, remove the first 6 lines from the output, get the ninth column, and get the CPU value in the second row.
+         top -p$2 -bn1 | awk 'NR>6 {print $9}' | sed -n '2 p';
     done
 
 }
